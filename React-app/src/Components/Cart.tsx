adfe8198c3b2c16d7fa4ee758e465d787287715f
Emmy <cyubahiroemmy12@gmail.com>
2026-02-04 20:41:33 +0200

    Updated

--- React-app/src/Components/Cart.tsx
+++ React-app/src/Components/Cart.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
 interface Props {
-    cartItems: string []
+    cartItems: string [];
+    onClear: () => void;
 }
 
-const Cart = ({cartItems}: Props) => {
+const Cart = ({cartItems, onClear}: Props) => {
   return (
     <>
     <div>Cart</div>
     <ul>
         {cartItems.map(item => <li key={item}>{item}</li>)}
     </ul>
+    <button onClick={onClear}>Clear</button>
     </>
   )
 }
