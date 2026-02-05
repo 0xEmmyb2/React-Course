61d37e9c7d4013f7776ba389d5511558b4fdd9ed
Emmy <cyubahiroemmy12@gmail.com>
2026-02-05 08:03:49 +0200

    Updated

--- React-app/src/Components/ExpandableText.tsx
+++ React-app/src/Components/ExpandableText.tsx
@@ -9,7 +9,7 @@ const ExpandableText = ({children, maxChars=100}: Props) => {
   const [isExpanded, setExpanded] = useState(false);
     if(children.length <= maxChars) return <p>{children}</p>
 
-    const text = children.substring(0, maxChars);
+    const text = isExpanded ? children :  children.substring(0, maxChars);
   return (
     <p>{text}... <button onClick={() => setExpanded(!isExpanded)}>{isExpanded ? 'Less' : 'More'}</button></p>
   )
