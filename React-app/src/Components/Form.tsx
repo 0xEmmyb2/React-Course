ab130f0d842627383c855861fe7af8a5f0a60b05
Emmy <cyubahiroemmy12@gmail.com>
2026-02-08 21:54:15 +0200

    Updated

--- React-app/src/Components/Form.tsx
+++ React-app/src/Components/Form.tsx
@@ -6,7 +6,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 //Schema validation
 const schema =z.object({
     name: z.string().min(3 , { message: 'Name must be atleast 3 character(s)'}),
-    age: z.number().min(18, { message: 'You must be 18 years or older'})
+    age: z.number( {invalid_type_error: 'Age field is required.'}).min(18, { message: 'You must be 18 years or older'})
 });
 
 type FormData = z.infer<typeof schema>
