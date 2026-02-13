1620021aa9c42c5c9e091016582a4e7d2f3a57f2
Emmy <cyubahiroemmy12@gmail.com>
2026-02-13 12:18:51 +0200

    Updated

--- React-app/src/expense-tracker/components/ExpenseForm.tsx
+++ React-app/src/expense-tracker/components/ExpenseForm.tsx
@@ -13,7 +13,7 @@ const schema = z.object({
 
 type ExpenseFormData = z.infer<typeof schema>;
 
-const FormData = () => {
+const ExpenseForm = () => {
   const {
     register,
     handleSubmit,
@@ -23,42 +23,44 @@ const FormData = () => {
     resolver: zodResolver(schema),
     mode: "onChange",
   });
-};
+
 
 const onSubmit = (data: ExpenseFormData) => {
   console.log("Submitted expenses: ", data);
   reset();
 };
 
-return (
-  <form onSubmit={handleSubmit(onSubmit)}>
-    <div className="mb-3">
-      <label htmlFor="description" className="form-label">
-        Description
-      </label>
-      <input id="description" type="text" className="form-control" />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="amount" className="form-label">
-        Amount
-      </label>
-      <input id="amount" type="number" className="form-control" />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="category" className="form-label">
-        Category
-      </label>
-      <select id="category" className="form-select">
-        <option value="">All Categories</option>
-        {categories.map((category) => (
-          <option key={category} value={category}>
-            {category}
-          </option>
-        ))}
-      </select>
-    </div>
-    <button className="btn btn-primary">Submit</button>
-  </form>
-);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">
+          Description
+        </label>
+        <input id="description" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input id="amount" type="number" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <select id="category" className="form-select">
+          <option value="">All Categories</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  );
+};
+
 
 export default ExpenseForm;
