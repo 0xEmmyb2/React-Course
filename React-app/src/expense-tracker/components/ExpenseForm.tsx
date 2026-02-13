0c36baa0d3ca5c58464adcae401b14d4a88591e4
Emmy <cyubahiroemmy12@gmail.com>
2026-02-13 12:14:00 +0200

    Updated

--- React-app/src/expense-tracker/components/ExpenseForm.tsx
+++ React-app/src/expense-tracker/components/ExpenseForm.tsx
@@ -30,37 +30,35 @@ const onSubmit = (data: ExpenseFormData) => {
   reset();
 };
 
-const ExpenseForm = () => {
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-3">
-        <label htmlFor="description" className="form-label">
-          Description
-        </label>
-        <input id="description" type="text" className="form-control" />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="amount" className="form-label">
-          Amount
-        </label>
-        <input id="amount" type="number" className="form-control" />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="category" className="form-label">
-          Category
-        </label>
-        <select id="category" className="form-select">
-          <option value="">All Categories</option>
-          {categories.map((category) => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-      </div>
-      <button className="btn btn-primary">Submit</button>
-    </form>
-  );
-};
+return (
+  <form onSubmit={handleSubmit(onSubmit)}>
+    <div className="mb-3">
+      <label htmlFor="description" className="form-label">
+        Description
+      </label>
+      <input id="description" type="text" className="form-control" />
+    </div>
+    <div className="mb-3">
+      <label htmlFor="amount" className="form-label">
+        Amount
+      </label>
+      <input id="amount" type="number" className="form-control" />
+    </div>
+    <div className="mb-3">
+      <label htmlFor="category" className="form-label">
+        Category
+      </label>
+      <select id="category" className="form-select">
+        <option value="">All Categories</option>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </div>
+    <button className="btn btn-primary">Submit</button>
+  </form>
+);
 
 export default ExpenseForm;
